42db806b62a01eeaa3050c2c8b2b7fdbf1cf76be
N. Omalley Segbee, Jr <116561836+segbeejr@users.noreply.github.com>
2023-04-20 12:28:20 +0000

    Final Commit

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('button');
 
-
-
 buttons.forEach((item) => {
-    item.onclick = () => {
-        if (item.id == 'clear') {
+    item.addEventListener('click', () => {
+        if (item.id === 'clear') {
             display.innerText = '';
-        } else if (item.id == 'delete') {
-            let string = display.innerText.tostring();
-            display.innerText = string.substr(0, string.length = 1);
-        } else if (display.innerText != '' && item.id == 'equal') {
+        } else if (item.id === 'delete') {
+            let string = display.innerText.toString();
+            display.innerText = string.substr(0, string.length - 1);
+        } else if (item.id === '÷') { // change division symbol to "÷"
+            display.innerText += '/';
+        } else if (item.id === '.') {
+            display.innerText += '.';
+        } else if (display.innerText !== '' && item.id === 'equal') {
             display.innerText = eval(display.innerText);
-        } else if (display.innerText == '' && item.id =='equal') {
+        } else if (display.innerText === '' && item.id === 'equal') {
             display.innerText = 'Empty!';
             setTimeout(() => (display.innerText = ''), 2000);
         } else {
             display.innerText += item.id;
         }
-    }
-})
+    });
+});
 
-
-
-const switchButton = document.getElementsByClassName('switchTheme');
-const bodyOne = document.querySelector('body');
+const switchButton = document.querySelector('.switchTheme');
 const body = document.body;
 
-
 switchButton.addEventListener('click', function() {
-  if (body.classList.contains('light-theme')) {
-    body.classList.remove('light-theme');
-    body.classList.add('dark-theme');
-  } else {
-    body.classList.remove('dark-theme');
-    body.classList.add('light-theme');
-  }
+    if (body.classList.contains('light-theme')) {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+    } else {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+    }
 });
